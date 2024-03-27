902a3c85717f363474f6974a1c24b6fbdd2d649f
KavinduGD <it21038914@my.sliit.lk>
2024-03-28 03:23:14 +0530

    daay 9

---
+++ day9/nexted_dic.py
@@ -0,0 +1,16 @@
+game_log={
+    "Assasins creed":
+        {
+        "names":["Brotherhood","Unity","Rouge","ACC III","Syndicate"],
+         "purchase":3,
+         "satisfaction":"High"
+         },
+    "COD":
+        {
+        "names":["Cod I","Cod II","Cod MW series", "WW II" ,"Cod 2019"],
+         "purchase":4,
+         "satisfaction":"High" 
+        }
+}
+
+print(game_log["Assasins creed"])
