8bf792f08cf415d5d62d15a1c07868bbead6ad1d
KavinduGD <it21038914@my.sliit.lk>
2024-03-29 02:49:18 +0530

    fix

--- day10/calculator.py
+++ day10/calculator.py
@@ -15,31 +15,32 @@ def division(n1,n2):
 
 print("===============Calculator==============\n")
 
-fist_number=None
+def calc_function():
+    fist_number=int(input("enter the first number - "))
 
-while True:
-    if fist_number==None:
-        fist_number=int(input("enter the first number - "))
-    else:
-        operator=input("What is the operator - ")
-        another_number=int(input("Enter another number - "))
-        
-        if operator=="+":
-            fist_number=addtion(fist_number,another_number)
-        elif operator=="-":
-            fist_number=subtraction(fist_number,another_number)
-        elif operator=="*":
-            fist_number=mutipication(fist_number*another_number)
-        else :
-            fist_number=division(fist_number,another_number)
+    while True:
+            operator=input("What is the operator - ")
+            another_number=int(input("Enter another number - "))
+            
+            if operator=="+":
+                fist_number=addtion(fist_number,another_number)
+            elif operator=="-":
+                fist_number=subtraction(fist_number,another_number)
+            elif operator=="*":
+                fist_number=mutipication(fist_number*another_number)
+            else :
+                fist_number=division(fist_number,another_number)
+                    
+            print(f"Here is the result {fist_number}")
+            
+            again=input("Do you want to continue - ")
+            
+            if again=="n":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                calc_function()
                 
-        print(f"Here is the result {fist_number}")
+
+calc_function()        
+         
+                
         
-        again=input("Do you want to continue - ")
-        
-        if again=="n":
-            fist_number=None
-            os.system('cls' if os.name == 'nt' else 'clear')
-            
-            
-    
